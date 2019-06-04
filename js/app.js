b67bc6fc1fd85d3b1d07e4b142fa67707a947497
pavel2593 <pavelborisov2593@gmail.com>
2019-06-04 17:53:24 +0300

    Все выполнено в соответствие с присланными правка. За исключением валидации.(10 пунтк функионала)

--- js/app.js
+++ js/app.js
@@ -1,28 +1,131 @@
-$(document).ready(function () {
-    $('.sidebar__btn').on('click', function () {
-        $('.wrapper__form_adding_new_feed').addClass('visibility');
-        $('.form_adding_new_feed').addClass('visibility');
-    });
+//AngularJS v1.7.8
 
-    $('.wrapper__form_adding_new_feed').on('click',function () {
-        $('.wrapper__form_adding_new_feed').removeClass('visibility');
-        $('.form_adding_new_feed').removeClass('visibility');
-        $('.form_editing_feed').removeClass('visibility');
-    });
+//Модуль
+var model = {
+    groups: [
+        {
+            name: "Програмирование",
+            id_color: "6",
+            article_links: [
+                {name: "Хабра", url: "http://habrahabr.ru/rss"},
+                {name: "Хабра 2", url: "http://habrahabr.ru/rss"},
+                {name: "Хабра 3", url: "http://habrahabr.ru/rss"}]
+        },
 
-    $('.form_adding_new_feed__cancel').on('click',function () {
-        $('.wrapper__form_adding_new_feed').removeClass('visibility');
-        $('.form_adding_new_feed').removeClass('visibility');
-        $('.form_editing_feed').removeClass('visibility');
-    });
+        {
+            name: "Дизайн",
+            id_color: "5",
+            article_links: [
+                {name: "Дизай", url: "дизайн.рф"}]
+        },
 
-    $('.form_adding_new_feed__add').on('click',function () {
-        $('.wrapper__form_adding_new_feed').removeClass('visibility');
-        $('.form_adding_new_feed').removeClass('visibility');
-    });
+        {
+            name: "Смешнявки",
+            id_color: "0",
+            article_links: [
+                {name: "Пикабу", url: "Пикабу.рф"}]
+        }],
 
-    $('.form_editing_feed__edit').on('click',function () {
-        $('.wrapper__form_adding_new_feed').removeClass('visibility');
-        $('.form_editing_feed').removeClass('visibility');
-    });
+    color_markers: [
+        {
+            className: 'color_red'
+        },
+
+        {
+            className: 'color_orange'
+        },
+
+        {
+            className: 'color_yellow'
+        },
+
+        {
+            className: 'color_green'
+        },
+
+        {
+            className: 'color_aqua'
+        },
+
+        {
+            className: 'color_blue'
+        },
+
+        {
+            className: 'color_purple'
+        }
+    ]
+};
+
+//Модуль
+var rssReader = angular.module("rssReaderApp", []);
+
+//Контроллер
+rssReader.controller("rssReaderCtrl", function ($scope) {
+    $scope.data = model;
+    $scope.indexGroup = 0;
+
+    //addNewGroup
+    $scope.groupName = "";
+    $scope.idGroupColor = "0";
+
+    //addNewLink
+    $scope.linkName = "";
+    $scope.linkUrl = "";
+
+    //editLink
+    $scope.editLinkName = "";
+    $scope.editLinkUrl = "";
+
+
+    $scope.showFormAdding = false;
+    $scope.showFormEditing = false;
+
+    //Обработчики нажатия
+    $scope.selectGroup = function () {
+        $scope.indexGroup = this.$index;
+    };
+
+    $scope.addNewGroup = function () {
+        $scope.data.groups.push({
+            name: $scope.groupName,
+            id_color: $scope.idGroupColor,
+            article_links: [],
+        });
+    };
+
+    $scope.deleteGroup = function () {
+        if (confirm("В данной группе находится " +$scope.data.groups[this.$index].article_links.length+ " ссылок. Вы уверены, что хотите удалить группу?")) {
+            $scope.data.groups.splice(this.$index, 1);
+        };
+    };
+
+    $scope.addNewLink = function () {
+        $scope.data.groups[$scope.indexGroup].article_links.push({
+            name: $scope.linkName,
+            url: $scope.linkUrl,
+        });
+
+        $scope.linkName = "";
+        $scope.linkUrl = "";
+    };
+
+    $scope.deleteLink = function () {
+        if (confirm("Вы уверены, что хотите удалить данную ссылку?")) {
+            $scope.data.groups[$scope.indexGroup].article_links.splice(this.$index, 1);
+        };
+    };
+
+
+    $scope.showFromEditing = function () {
+        $scope.editLinkName = $scope.data.groups[$scope.indexGroup].article_links[this.$index].name;
+        $scope.editLinkUrl = $scope.data.groups[$scope.indexGroup].article_links[this.$index].url;
+        $scope.showFormEditing = true;
+        $scope.indexEditLink = this.$index;
+        console.log($scope.indexEditLink, this.link.name, $scope.showFormEditing);
+    };
+
+    $scope.editLink = function () {
+        $scope.data.groups[$scope.indexGroup].article_links[$scope.indexEditLink] = {name: $scope.editLinkName, url: $scope.editLinkUrl};
+    };
 });
